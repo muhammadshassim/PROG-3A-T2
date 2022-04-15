47a195152e069bfda1f365be7cfa5e57fa00c256
Savage <Savage@DESKTOP-0C2TS0N>
2022-04-15 14:21:48 +0200

    Add project files.

---
+++ PROG-3A-T2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PROG_3A_T2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PROG-3A-T2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PROG_3A_T2.MvcApplication" Language="C#" %>
